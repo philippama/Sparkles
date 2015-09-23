7f7b4f206717cfb705bf8229746848d023ac2322
Philippa Main <philippa.main@autotrader.co.uk>
2015-09-23 08:10:15 +0100

    Added TextFileProcessingTest.

--- Java/SparkPlayground/src/test/java/pm/spark/playground/TextFileProcessingTest.java
+++ Java/SparkPlayground/src/test/java/pm/spark/playground/TextFileProcessingTest.java
@@ -3,21 +3,32 @@ package pm.spark.playground;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
+public class TextFileProcessingTest {
 
-public class Playground {
-    public static void main(String[] args) {
-        String logFile = "resources/sonnet.txt";
+    private static JavaSparkContext sc;
+
+    private static final String TEXT_FILE = "resources/sonnet.txt";
+
+    @BeforeClass
+    static public void setUp() {
         SparkConf conf = new SparkConf()
                 .setMaster("local[2]")
                 .setAppName("Simple Application");
         System.out.println("SparkConf: " + conf);
-        JavaSparkContext sc = new JavaSparkContext(conf);
-        JavaRDD<String> logData = sc.textFile(logFile).cache();
+        sc = new JavaSparkContext(conf);
+    }
+
+    @Test
+    public void test() {
+        JavaRDD<String> logData = sc.textFile(TEXT_FILE).cache();
 
         long numAs = logData.filter(line -> line.contains("a")).count();
         long numBs = logData.filter(line -> line.contains("b")).count();
 
         System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+
     }
 }
